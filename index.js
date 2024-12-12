6560c10877e8f1d09e3aa12838d8104817825831
Paulo Prado <paulroar@hotmail.com>
2024-12-12 17:24:19 +0000

    Done

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+      this.items.push(item);
+      this.items.sort((a,b) => a-b);
+      this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+   }
+   return this.items[this.length -1];
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0) {
+      throw new Error("Empty SortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
-
+  sum() {
+    let sum = 0;
+    for (let i = 0; i < this.length; i++) {
+      sum += this.items[i];
+    }
+  }
   avg() {}
 }
+
 
 module.exports = SortedList;
